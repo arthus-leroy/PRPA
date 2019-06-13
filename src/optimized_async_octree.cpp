8f420f849a40396b94cd3115bbc4418e0d204de4
Arthus Leroy <arthus.leroy@epita.fr>
2019-06-13 18:24:57 +0200

    [ASYNC][V1] optimized_async_octree: correct and partition code

--- src/optimized_async_octree.cpp
+++ src/optimized_async_octree.cpp
@@ -1,215 +1,41 @@
-# include <strings.h>
-# include <queue>
-
 # include "optimized_async_octree.hpp"
 
 template <typename T, unsigned N>
-node<T, N>::node()
-    : free(-1ULL)
-{}
-
-template <typename T, unsigned N>
-AsyncOptimizedQuadTree<T, N>::AsyncOptimizedQuadTree(const std::initializer_list<T>& init)
+AsyncOptimizedOcTree<T, N>::AsyncOptimizedOcTree(const std::initializer_list<T>& init)
 {
     this->init(init);
 }
 
 template <typename T, unsigned N>
-void AsyncOptimizedQuadTree<T, N>::init(const std::initializer_list<T>& init)
+void AsyncOptimizedOcTree<T, N>::init(const std::initializer_list<T>& init)
 {
     for (const auto&  elem : init)
-        insert(elem);
-}
-
-namespace
-{
-    inline long distance(const point a, const point b)
-    {
-        long x = a.x - b.x;
-        long y = a.y - b.y;
-        long z = a.z - b.z;
-
-        return x*x + y*y + z*z;
-    }
-
-    /// return index of direction corresponding to corrdinates in a cube
-    inline int get_dir(const point a, const point b)
-    {
-        return 4 * ((a.z - b.z) >= 0)
-             + 2 * ((a.y - b.y) >= 0)
-             + 1 * ((a.x - b.x) >= 0);
-    }
+        octree_.insert(elem);
 }
 
 template <typename T, unsigned N>
 std::future<result_t>
-AsyncOptimizedQuadTree<T, N>::search(const T e) const noexcept
+AsyncOptimizedOcTree<T, N>::search(const T e) const noexcept
 {
-    // previous node's lock
-    std::mutex prev;
-    // return value
-    std::promise<result_t> ret;
-    // nearest element found
-    T nearest;
-    // queue of nodes for breadth-first search
-    std::queue<struct node<T, N>*> nodes;
-
-    // distance between the nearest element found and the target
-    unsigned dist = -1U;
-    // current node
-    auto& n = root_;
-
-
-    while (!nodes.empty())
-    {
-        n = nodes.pop();
-
-        // lock current node
-        n.m.lock();
-        // release previous node
-        prev.unlock();
-
-        for (const auto elem : n.elems)
-        {
-            int d = distance(elem, e);
-            if (d < dist)
-            {
-                nearest = elem;
-                dist = d;
-            }
-        }
-
-        // optimization: element found, no need to go any further
-        if (dist == 0)
-        {
-            ret.set_value(nearest);
-            return ret.get_future();
-        }
-
-        prev = n.m;
-        for (const auto* node : n.nodes)
-        // TODO: insert a test to eliminate nodes outside of rande (> distance)
-            nodes.push(node);
-    }
-
-    n.m.unlock();
-
-    // element found near position
-    if (dist != -1U)
-        ret.set_value(nearest);
-
-    return ret.get_future();
+    std::promise<result_t> promise;
+    promise.set_value(octree_.search(e));
+    return promise.get_future();
 }
 
 template <typename T, unsigned N>
 std::future<void>
-AsyncOptimizedQuadTree<T, N>::insert(const T e) noexcept
+AsyncOptimizedOcTree<T, N>::insert(const T e) noexcept
 {
-    // previous node's lock
-    std::mutex prev;
-    // return value
-    std::promise<void> ret;
-
-    // current node
-    auto& n = root_;
-
-    while (true)
-    {
-        // lock current node
-        n.m.lock();
-        // release previous node
-        prev.unlock();
-
-        // FIXME: need to descend further to check if point isn't set somewhere
-        for (const auto elem : n.elems)
-            // already in tree, so don't go any further
-            if (e == elem)
-            {
-                n.m.unlock();
-                return ret.get_future();
-            }
-
-
-        // free slot(s)
-        if (n.free.any())
-        {
-            // ffsll return least significant bit, N - i is most significant
-            const int i = ffsll(n.free.to_ullong());
-            n.elems[N - i] = e;
-            n.free[i - 1] = false;
-
-            // value inserted
-            n.m.unlock();
-            return ret.get_future();
-        }
-
-        // find next node
-        auto dir = get_dir(e, n.center);
-        prev = n.m;
-
-        auto pt = n.nodes[dir];
-        if (pt != nullptr)
-        {
-            pt = std::make_unique<struct node<T, N>>();
-            n.nodes[dir] = pt;
-        }
-
-        n = *pt;
-    }
-
-    return ret.get_future();
+    std::promise<void> promise;
+    promise.set_value(octree_.insert(e));
+    return promise.get_future();
 }
 
 template <typename T, unsigned N>
 std::future<void>
-AsyncOptimizedQuadTree<T, N>::erase(const T e) noexcept
+AsyncOptimizedOcTree<T, N>::erase(const T e) noexcept
 {
-    // previous node's lock
-    std::mutex prev;
-
-    // return value
-    std::promise<void> ret;
-    // current node
-    auto& n = root_;
-
-    while (true)
-    {
-        // lock current node
-        n.m.lock();
-        // release previous node
-        prev.unlock();
-
-        int i;
-        auto used = (~n.free).to_ullong();
-        for (int pos = -1; used; used >>= i)
-        {
-            // ffsll return least significant bit, N - i is most significant
-            i = ffsll(used);
-            pos += i;
-            if (e == n.elems[N - pos])
-            {
-                n.free[pos - 1] = true;
-
-                // value deleted
-                n.m.unlock();
-                return ret.get_future();
-            }
-        }
-
-        // find next node
-        auto dir = get_dir(e, n.center);
-        prev = n.m;
-
-        const auto pt = n.nodes[dir];
-        // no point in the targeted corner, aborting
-        if (pt != nullptr)
-        {
-            prev.unlock();
-            return ret.get_future();
-        }
-
-        n = *pt;
-    }
-
-    return ret.get_future();
+    std::promise<void> promise;
+    promise.set_value(octree_.erase(e));
+    return promise.get_future();
 }
--- src/optimized_async_octree.hpp
+++ src/optimized_async_octree.hpp
@@ -3,37 +3,13 @@
 # include <memory>
 
 # include "IAsyncQuadTree.hpp"
-
-/** TODO:
- *  ask how max number of elements (for possiblity of array based implem)
- *  ask max size of cube (for size of sub-cubes, since a sub-cube)
- *  verify if fairness is ensured
- */
+# include "optimized_octree.hpp"
 
 template <typename T, unsigned N>
-struct node
-{
-    node();
-
-    /// mutex to block the node if busy
-    std::mutex m;
-    // TODO: test the trade-off 16x16x16, 8x8x8 or 4x4x4 compared to 2x2x2
-    /// pointers to the 8 corners of the octotree
-    std::array<std::unique_ptr<node*>, 8> nodes;
-    /// elements contained in the leaf
-    std::array<T, N> elems;
-    /// map of slots used in the node (1 = used)
-    std::bitset<N> free;
-    /// coordinate of the center of the sub-octree
-    struct point center;
-};
-
-template <typename T, unsigned N>
-class AsyncOptimizedQuadTree : IAsyncQuadTree
+class AsyncOptimizedOcTree : IAsyncQuadTree
 {
 public:
-    AsyncOptimizedQuadTree(const std::initializer_list<T>& init);
-    ~AsyncOptimizedQuadTree();
+    AsyncOptimizedOcTree(const std::initializer_list<T>& init = {});
 
     void init(const std::initializer_list<T>& init);
     std::future<result_t> search(const T elem) const noexcept final;
@@ -41,5 +17,5 @@ public:
     std::future<void> erase(const T elem) noexcept final;
 
 private:
-    struct node<T, N> root_;
+    OptimizedOcTree<T, N> octree_;
 };
